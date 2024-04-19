4b57f96bbf0f51fe20b35edef02e98ca2f153a62
Peacechimamaka <peaceamaka111@gmail.com>
2024-04-19 12:15:51 -0700

    puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # A Puppete manifest that install flask from pip3
 package { 'flask':
-  ensure => '2.1.0',
+  ensure   => '2.1.0',
   provider => 'pip3'
 }
 
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # A puppet manifest that kills a process named killmenoe
 exec { 'killmenow_process':
-  command => '/usr/bin/pkill killmenow',
+  command     => '/usr/bin/pkill killmenow',
   refreshonly => true
 }
 
