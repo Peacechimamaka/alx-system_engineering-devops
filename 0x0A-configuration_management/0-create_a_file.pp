a0f59c542d2186168a888827df978996e81f70c7
Peacechimamaka <peaceamaka111@gmail.com>
2024-04-19 11:54:41 -0700

    puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# A puppet file create a file school in tmp dir
+
+file { '/tmp/school' :
+	ensure => file,
+	mode => '0744',
+	owner => 'www-data',
+	group => 'www-data',
+	content => 'I love Puppet',
+}
+
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,6 @@
+# A puppet manifest that kills a process named killmenoe
+exec { 'killmenow_process':
+	command => '/usr/bin/pkill killmenow',
+	refreshonly => true,
+}
+
