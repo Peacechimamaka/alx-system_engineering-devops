d179572ef829cb5b229d12767893737b96c99a1b
Peacechimamaka <peaceamaka111@gmail.com>
2024-06-04 16:58:57 +0100

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,28 +1,19 @@
 #!/usr/bin/python3
 """
-A function queries Reddit API and returns the number of subscribers
+Script that queries subscribers on a given Reddit subreddit.
 """
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the total number of subscribers
-    for a subreddit given.
-    """
-    url_b = 'https://www.reddit.com'
-    uri_a = '{base}/r/{subreddit}/about.json'.format(base=url_b,
-                                                       subreddit=subreddit)
-
-    # Set an User-Agent
-    agent = {'User-Agent': 'Python/requests'}
-
-    # Get the Response of the Reddit API
-    response = requests.get(uri_a, headers=agent,
-                       allow_redirects=False)
-
-    # If the subreddit is invalid
-    if response.status_code in [302, 404]:
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
         return 0
-
-    # Returning the total subscribers of the subreddit
-    return response.json().get('data').get('subscribers')
